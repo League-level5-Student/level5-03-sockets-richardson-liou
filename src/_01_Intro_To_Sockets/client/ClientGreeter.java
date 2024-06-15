b2dacd6fd510b13371fcd74638dbeb4d53ece6f8
league <league@imac-15.attlocal.net>
2024-06-15 11:31:38 -0700

    start mod 3

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -8,7 +8,7 @@ public class ClientGreeter {
    public static void main(String [] args) {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
+      String ip = "192.168.1.213";
       //2. Create an integer for the server's port number
       
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
